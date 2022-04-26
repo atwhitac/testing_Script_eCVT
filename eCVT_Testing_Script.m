e01ef2c8f4dd8212d00cc86ceaa8d389d90edbf3
BenLeistiko <38890737+BenLeistiko@users.noreply.github.com>
2022-04-25 20:07:59 -0700

    Change to latest version of testing script

--- eCVT_Testing_Script.m
+++ eCVT_Testing_Script.m
@@ -1,76 +1,137 @@
 % eCVT Testing Script
+global lookuptable;
+lookuptable = table2array(readtable('eCVT Encoder Lookup Table.csv','NumHeaderLines',1)); %<--- Lookup table .csv
+global testData;
+testData = 'P0.03I0.2D0.002.csv';  %<--- This is where input .csv goes
+global clampingForceFOS;
+clampingForceFOS = 0.5;
+global highRatio;
+highRatio = 4.496; %0 Ratio Percentage
+global lowRatio;
+lowRatio = 0.857; %100 Ratio Percentage
 
 close all
-%% Specify Data Location
-time           = column(1);
-time = (round((time-time(1))/10000))/100;
-engineEngaged  = column(2);         
-engineState    = column(3);  
-engineSpeed    = column(4);            
-enginePID      = column(5);  
-engineP        = column(6);
-engineI        = column(7);
-engineD        = column(8);
-primaryState   = column(9);
-primaryEnc     = column(10);
-primaryLC      = column(11);
-primaryPID     = column(12);
-secondaryState = column(13);
-secondaryEnc   = column(14);
-secondaryLC    = column(15);
-secondaryPID   = column(16);
-engineSpeedSet = 3500*ones(length(time), 1);
+%% Constants
 
-secondarymaxLC = max(abs(secondaryLC))
-primarymaxLC = max(abs(primaryLC))
+
+
+
+
+%% Specify Data Location
+time               = column(1);
+time               = (round((time-time(1))/10000))/100;
+sSpeed             = column(2)*6.95;
+carSpeed           = column(2)*3.1415*1.916/60; % ft/s     
+
+% Engine
+eState             = column(3);  
+eSpeed             = column(4);            
+ePID               = column(5);  
+eP                 = column(6); 
+eI                 = column(7);
+eD                 = column(8);
+
+% Primary
+pState             = column(9);
+pEncoder           = column(10);
+pClampForce        = column(11)*1.8333;
+pMotorCurrent      = column(12)/43.4;
+pControllerOutput  = column(13);
+
+%Secondary
+sState             = column(14);
+sEncoder           = column(15);
+sClampForce        = column(16)*-1.8333;
+sMotorCurrent      = column(17)/43.4;
+sControllerOutput  = column(18);
+sEncoderPID        = column(19);
+sLoadCellPID       = column(20);
+sLoadCellP         = column(21);
+sLoadCellI         = column(22);
+sLoadCellD         = column(23);
+
+slipRatio = zeros(length(time));
+i = 1;
+while(i<length(time))
+    if(eState(i) > 1)
+       slipRatio(i) = sSpeed(i) / (eSpeed(i)/pEncoderToRatio(pEncoder(i))) - 1;
+    end
+    i = i + 1;
+end
+
+d1 = designfilt("lowpassiir",FilterOrder=12, ...
+    HalfPowerFrequency=0.15,DesignMethod="butter");
+filteredSlipRatio = filtfilt(d1,slipRatio);
+
+carDistance = cumtrapz(filtfilt(d1, carSpeed))/100; % ft
+
+eSetPoint          = 3500 * ones(length(time), 1);
+PIDZeroMark        = zeros(length(time), 1);
+
+ratioPercentage    = abs(floor(ePID));
+pEncoderTarget     = ratioPercentageToPEncoder(ratioPercentage);
+sEncoderTarget     = ratioPercentageToSEncoder(ratioPercentage);
+clampForceTarget   = ratioPercentageToClampForce(ratioPercentage);
+
+secondaryMaxClamp  = max(abs(sClampForce))
+primaryMaxClamp    = max(abs(pClampForce))
+engineSpeedMax     = max(abs(eSpeed))
+
+primaryAverageDraw = mean(pMotorCurrent(interp1(time,1:length(time),13,'nearest'):interp1(time,1:length(time),18,'nearest')))
+secondaryAverageDraw = mean(sMotorCurrent(interp1(time,1:length(time),13,'nearest'):interp1(time,1:length(time),18,'nearest')))
+ratio = primaryAverageDraw/secondaryAverageDraw
+AverageRPM = mean(eSpeed(interp1(time,1:length(time),2,'nearest'):interp1(time,1:length(time),17,'nearest')))
+
 
 %% Engine Loop
 engineLoopFig = figure('Name', 'Engine Loop Data', 'NumberTitle', 'off');
-engineError = engineSpeedSet-engineSpeed;
-engineData = array2table([time engineSpeed engineSpeedSet engineP engineI engineD enginePID engineError engineState]);
-engineData = renamevars(engineData, ["Var1" "Var2" "Var3" "Var4" "Var5" "Var6" "Var7" "Var8" "Var9"], ["Time" "Engine Speed Actual" "Engine Speed Setpoint" "Engine P" "Engine I" "Engine D" "Engine PID" "Engine Error" "Engine State"]);
-engineVars = {'Engine State',{'Engine Speed Actual', 'Engine Speed Setpoint'}, 'Engine Error',{'Engine P', 'Engine I','Engine D','Engine PID'}};
+engineData = array2table([time eSpeed eSetPoint eP eI eD ePID eState carSpeed carDistance]);
+engineData = renamevars(engineData, ["Var1" "Var2" "Var3" "Var4" "Var5" "Var6" "Var7" "Var8" "Var9" "Var10"], ["Time" "Engine Speed" "Engine Speed Setpoint" "Engine P" "Engine I" "Engine D" "Engine PID" "Engine State" "Car Speed" "Car Distance"]);
+engineVars = {'Engine State',{'Engine Speed', 'Engine Speed Setpoint'}, {'Engine P', 'Engine I','Engine D','Engine PID'}, 'Car Speed', 'Car Distance'};
 engineLoopTL = stackedplot(engineData, engineVars, 'XVariable','Time');
+engineLoopTL.AxesProperties(2).YLimits = [3000 4000];
+engineLoopTL.AxesProperties(3).YLimits = [0 100];
+engineLoopTL.AxesProperties(4).YLimits = [0 40];
 
 
 %% Primary Loop
-ratioPercentage = abs(floor(enginePID));
-primaryEncTarget = lookup(ratioPercentage, 2);
-primaryError = primaryEncTarget-primaryEnc;
+
 
 primaryLoopFig = figure('Name', 'Primary Loop Data', 'NumberTitle', 'off');
-primaryData = array2table([time primaryEnc primaryEncTarget primaryPID primaryError primaryState]);
-primaryData = renamevars(primaryData, ["Var1" "Var2" "Var3" "Var4" "Var5" "Var6"], ["Time" "Primary Encoder Ticks" "Primary Encoder Ticks Target" "Primary PID" "Primary Error" "Primary State"]);
-primaryVars = {'Primary State', {'Primary Encoder Ticks', 'Primary Encoder Ticks Target'}, 'Primary Error', 'Primary PID'};
+primaryData = array2table([time pEncoder pEncoderTarget pControllerOutput PIDZeroMark pState pClampForce clampForceTarget pMotorCurrent]);
+primaryData = renamevars(primaryData, ["Var1" "Var2" "Var3" "Var4" "Var5" "Var6" "Var7" "Var8" "Var9"], ["Time" "Primary Encoder Ticks" "Primary Encoder Ticks Target" "Primary PID" "PIDZeroMark" "Primary State" "Primary Force" "Target Force" "Motor Current"]);
+primaryVars = {'Primary State', {'Primary Encoder Ticks', 'Primary Encoder Ticks Target'}, {'Primary PID', 'PIDZeroMark'}, {'Primary Force', 'Target Force'}, 'Motor Current'};
 primaryLoopTL = stackedplot(primaryData, primaryVars, 'XVariable','Time');
 
 
 %% Secondary Loop
-secondaryEncTarget = lookup(ratioPercentage, 3);
-secondaryError = secondaryEncTarget-secondaryEnc;
 
 secondaryLoopFig = figure('Name', 'Secondary Loop Data', 'NumberTitle', 'off');
-secondaryData = array2table([time secondaryEnc secondaryEncTarget secondaryPID secondaryError secondaryState]);
-secondaryData = renamevars(secondaryData, ["Var1" "Var2" "Var3" "Var4" "Var5" "Var6"], ["Time" "Secondary Encoder Ticks" "Secondary Encoder Ticks Target" "Secondary PID" "Secondary Error" "Secondary State"]);
-secondaryVars = {'Secondary State', {'Secondary Encoder Ticks', 'Secondary Encoder Ticks Target'}, 'Secondary Error', 'Secondary PID'};
+secondaryData = array2table([time sEncoder sEncoderTarget sControllerOutput sEncoderPID sLoadCellPID sLoadCellP sLoadCellI sLoadCellD PIDZeroMark  sState sClampForce clampForceTarget sMotorCurrent filteredSlipRatio]);
+secondaryData = renamevars(secondaryData, ["Var1" "Var2" "Var3" "Var4" "Var5" "Var6" "Var7" "Var8" "Var9" "Var10" "Var11" "Var12" "Var13" "Var14" "Var15"], ["Time" "Secondary Encoder Ticks" "Secondary Encoder Ticks Target" "Secondary Controller Output" "sEncoderPID" "sLoadCellPID" "sLoadCellP" "sLoadCellI" "sLoadCellD" "PIDZeroMark" "Secondary State" "Secondary Force" "Target Force" "Motor Current" "Slip Ratio"]);
+secondaryVars = {'Secondary State', {'Secondary Encoder Ticks', 'Secondary Encoder Ticks Target'}, {'Secondary Controller Output', 'sEncoderPID', 'sLoadCellPID', 'PIDZeroMark'}, {'sLoadCellP', 'sLoadCellI', 'sLoadCellD', 'PIDZeroMark'}, {'Secondary Force', 'Target Force'}, 'Motor Current', 'Slip Ratio'};
 secondaryLoopTL = stackedplot(secondaryData, secondaryVars, 'XVariable','Time');
+secondaryLoopTL.AxesProperties(7).YLimits = [-.3 .3];
 
 
+%% Load Cell Forces
 
+ForceFig = figure('Name', 'Load Cell Forces', 'NumberTitle', 'off');
+plot(time, pClampForce);
+hold on;
+plot(time, -sClampForce);
+hold off;
+legend("Primary", "Secondary");
 
-%% Secondary Force Loop
+%% Load Cell Forces
 
-secondaryForceLoopFig = figure('Name', 'Secondary Load Cell Force', 'NumberTitle', 'off');
-plot(time, secondaryLC);
-
-
+%ForceFigFiltered = figure('Name', ['Load Cell Force Filtered'], 'NumberTitle', 'off');
+%plot(time, filter([1000, 1000],1,transpose(primaryLC+secondaryLC)));
 
 
 %% User Defined Functions
 function [x] = column(n)
-
-testData = 'ben hill.csv';  %<--- This is where input .csv goes
-
+global testData;
 T = table2array(readtable(testData, 'NumHeaderLines',0));
 if n <= 0
     x = zeros(length(T(:,1)),1);
@@ -79,8 +140,70 @@ else
 end
 end
 
-function [x] = lookup(n1,n2) % Lookup Table Function
-lookuptable = table2array(readtable('eCVT Encoder Lookup Table.csv','NumHeaderLines',1)); %<--- Lookup table .csv
-x = lookuptable(101-n1,n2);
+function [x] = ratioPercentageToSEncoder(ratioPercentage) % Lookup Table Function
+global lookuptable;
+if(ratioPercentage > 100)
+    ratioPercentage = 100;
+end
+if(ratioPercentage < 0)
+    ratioPercentage = 0;
+end
+x = lookuptable(101-ratioPercentage,3);
+
+end
+
+function [x] = ratioPercentageToPEncoder(ratioPercentage) % Lookup Table Function
+global lookuptable;
+if(ratioPercentage > 100)
+    ratioPercentage = 100;
+end
+if(ratioPercentage < 0)
+    ratioPercentage = 0;
+end
+x = lookuptable(101-ratioPercentage,2);
+
+end
+
+function ratio = pEncoderToRatio(pEncoder) % Lookup Table Function
+global lookuptable;
+global highRatio;
+global lowRatio;
+i = 1;
+if(pEncoder < 8010)
+    ratio = lowRatio;
+elseif(pEncoder > 48295)
+       ratio = highRatio;
+else
+    while(i<102)
+        if(lookuptable(i,2)>pEncoder)
+            break
+        end
+        i = i + 1;
+    end
+
+    pEncoder1 = lookuptable(i-1,2);
+    pEncoder2 = lookuptable(i,2);
+
+    ratioPercent1 = lookuptable(i-1,1);
+    ratioPercent2 = lookuptable(i,1);
+
+    ratioPercent = ratioPercent1 + (ratioPercent2-ratioPercent1) * ( (pEncoder - pEncoder1) / (pEncoder2-pEncoder1) );
+
+    ratio = highRatio - ((highRatio-lowRatio)*(1-ratioPercent*0.01));
+
+    end
+end
+
+function [x] = ratioPercentageToClampForce(ratioPercentage) % Lookup Table Function
+global lookuptable;
+global clampingForceFOS;
+x = lookuptable(101-ratioPercentage,4) * clampingForceFOS;
+
+end
+
+
+function [dist] = getDistanceSpeed(rearWheelSpeed)
+dist = zeros(length(rearWheelSpeed));
+
 
 end
